1037eed7c7f447a7ed93ac631c07a6d9094f66a7
Alitux <alejandrofabrega@gmail.com>
2014-08-21 20:00:17 -0300

    Se crea el script básico montarextraer.sh Cuidado: ¡Todavía no está testeado! ¡TESTEAR!

--- scripts/montarextraer.sh
+++ scripts/montarextraer.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 #Script para extraer y montar la imagen a modificar
+#Se crean las carpetas de trabajo
 
-#
+#Se crea carpeta de trabajo
+mkdir $2
+#Se crea carpeta mnt
+mkdir $2/mnt
+#Se crea carpeta extract-cd
+mkdir $2/extract-cd
+
+#Se crea la carpeta edit
+mkdir $2/edit
+
+#Se monta la ISO en modo loop sobre mnt
+sudo mount -o loop $1 $2/mnt
+
+#Se extrae el contenido de la ISO en la carpeta de trabajo
+sudo rsync --exclude=/casper/filesystem.squashfs -a $2/mnt/ $2/extract-cd
+
+#Se extrae el sistema de escritorio en $2/edit
+sudo unsquashfs $2/mnt/casper/filesystem.squashfs
+sudo mv squashfs-root $2/edit
